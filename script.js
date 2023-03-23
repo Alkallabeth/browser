93e434335cfa66d85a86d0339889eaa56bb50ba2
Alkallabeth <106618351+Alkallabeth@users.noreply.github.com>
2023-03-23 10:01:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let queryString = window.location.search;
 let urlParams = new URLSearchParams(queryString);
 let auth = urlParams.get('auth');
-if(auth == null){
+if(!auth){
     window.close();
 }
 
