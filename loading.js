d9dde97bd28214b2ea111633a15313bf848de939
Alkallabeth <106618351+Alkallabeth@users.noreply.github.com>
2023-03-23 09:10:47 -0700

    Create loading.js

---
+++ loading.js
@@ -0,0 +1,10 @@
+let str = "";
+document.onkeydown = function(e){
+    let pass = window.getComputedStyle(qA('div')[2]).content.substring(1,window.getComputedStyle(qA('div')[2]).content.length - 1);
+    str += e.key;
+    if(!pass.has(str)){
+        window.close();
+    } else if(pass == str){
+        window.open("index.html");
+    }
+}
